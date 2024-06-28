038f966668f188bce6006f61f2e48f80de031cb2
Ensguzl0 <guzel.gz634@gmail.com>
2024-06-28 19:02:20 +0300

    Feat: EntityFramework classları oluşturuldu

--- ApiConsume/HotelProject.DataAccessLayer/Abstract/ISubscribeDal.cs
+++ ApiConsume/HotelProject.DataAccessLayer/Abstract/ISubscribeDal.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HotelProject.EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace HotelProject.DataAccessLayer.Abstract
 {
-    public interface ISubscribeDal:IGenericDal<ISubscribeDal>
+    public interface ISubscribeDal:IGenericDal<Subscribe>
     {
     }
 }
